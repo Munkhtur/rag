07c1306b5f45e1309cc06b13c3181514cffa1c13
Munkhtur Galbadrakh <munkhtur.g@gerege.local>
2025-02-12 18:39:46 +0800

    212-2

--- json_test.py
+++ json_test.py
@@ -2,9 +2,9 @@ import json
 import os
 from sentence_transformers import InputExample
 # Path to the JSON file
-file_path = "./books/mongol_data.jsonl"
+file_path = "./scraper/data/training_data.jsonl"
 # file_path_res = "./books/result.json"
-output_file = "./books/sample_pairs_2.txt"  
+output_file = "./sample_pairs_3.txt"  
 # Load existing data from the JSON file, if it exists
 if os.path.exists(file_path):
     with open(file_path, "r") as f:
@@ -22,16 +22,16 @@ for query_id, query in queries.items():
     examples.append(example)
 
 print(len(queries))
-# with open(output_file, "w", encoding="utf-8") as out_file:
-#     for query_id, query in queries.items():
-#         # Get the relevant document using the query_id
-#         doc_id = relevant_docs[query_id][0]
-#         text = corpus[doc_id]
+with open(output_file, "w", encoding="utf-8") as out_file:
+    for query_id, query in queries.items():
+        # Get the relevant document using the query_id
+        doc_id = relevant_docs[query_id][0]
+        text = corpus[doc_id]
         
-#         # Write the question and relevant document pair in a readable format
-#         out_file.write(f"Question: {query}\n")
-#         out_file.write(f"Relevant Document: {text}\n")
-#         out_file.write("-" * 50 + "\n")  # Divider between each pair
+        # Write the question and relevant document pair in a readable format
+        out_file.write(f"Question: {query}\n")
+        out_file.write(f"Relevant Document: {text}\n")
+        out_file.write("-" * 50 + "\n")  # Divider between each pair
 
 print(f"Question and relevant document pairs saved to {output_file}!")
 
